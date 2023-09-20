5b5e9dbadcaecb8300d1690dc4a00b7012c4910e
Daniel Keefe <dfk@umn.edu>
2023-09-20 17:56:55 -0500

    updates

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -1,29 +1,146 @@
 /** CSci-4611 Example Code
  * Copyright 2023+ Regents of the University of Minnesota
  * Please do not distribute beyond the CSci-4611 course
+ * 
+ * This example created by Prof. Evan Suma Rosenberg.
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  */
 
 import * as gfx from 'gophergfx'
-
+import { SpriteCharacter } from './SpriteCharacter';
 
 export class ExampleApp extends gfx.GfxApp
-{   
-    // --- Create the ExampleApp class ---
+{
+    private character: SpriteCharacter;
+    private cameraControls: gfx.OrbitControls;
+
     constructor()
     {
-        // initialize the base class gfx.GfxApp
         super();
+
+        this.cameraControls = new gfx.OrbitControls(this.camera);
+        this.character = new SpriteCharacter(1, 512/360, 40/360, 30);
     }
 
-
-    // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        // Setup camera
+        this.camera.setPerspectiveCamera(60, 1920/1080, 1, 800);
+        this.cameraControls.setTargetPoint(new gfx.Vector3(0, 0, 0));
+        this.cameraControls.setOrbit(gfx.MathUtils.degreesToRadians(-30), gfx.MathUtils.degreesToRadians(-135));
+        this.cameraControls.rotationSpeedX = 0;
+        this.cameraControls.setDistance(12);
+
+        // Set a black background
+        this.renderer.background.set(0, 0, 0);
+        
+        // Create an ambient light
+        const ambientLight = new gfx.AmbientLight(new gfx.Vector3(0.6, 0.6, 0.6));
+        this.scene.add(ambientLight);
+
+        // Create a directional light
+        const directionalLight = new gfx.DirectionalLight(new gfx.Vector3(0.6, 0.6, 0.6));
+        directionalLight.position.set(1, 1, 1)
+        this.scene.add(directionalLight);
+
+        // Create a sky sphere
+        const sky = gfx.Geometry3Factory.createSphere(400, 1);
+        sky.material = new gfx.UnlitMaterial();
+        sky.material.setColor(new gfx.Color(0.698, 1, 1));
+        sky.material.side = gfx.Side.BACK;
+        this.scene.add(sky);
+
+        // Create a sky sphere
+        const ground = gfx.Geometry3Factory.createPlane(400, 400);
+        ground.rotation = gfx.Quaternion.makeRotationX(Math.PI/2);
+        ground.material.setColor(new gfx.Color(.375, .476, .198));
+        this.scene.add(ground);
+
+        const rpgMaterial = new gfx.GouraudMaterial();
+        rpgMaterial.texture = new gfx.Texture('./assets/textures/rpgpp_lt_tex_a.png');
+
+        const terrain = gfx.MeshLoader.loadOBJ('./assets/meshes/terrain.obj');
+        terrain.position.y = -1;
+        terrain.material = rpgMaterial;
+        this.scene.add(terrain);
+
+        const buildings1 = gfx.MeshLoader.loadOBJ('./assets/meshes/buildings1.obj');
+        buildings1.material = rpgMaterial;
+        this.scene.add(buildings1);
+
+        const buildings2 = gfx.MeshLoader.loadOBJ('./assets/meshes/buildings2.obj');
+        buildings2.material = rpgMaterial;
+        this.scene.add(buildings2);
+
+        const vegetation = gfx.MeshLoader.loadOBJ('./assets/meshes/vegetation.obj');
+        vegetation.position.y = 0.5;
+        vegetation.material = rpgMaterial;
+        this.scene.add(vegetation);
+
+        const props = gfx.MeshLoader.loadOBJ('./assets/meshes/props.obj');
+        props.position.y = 1;
+        props.material = rpgMaterial;
+        this.scene.add(props);
+
+        this.character.position.set(-5, 0, 2);
+        this.character.scale.set(2, 2, 1);
+        this.scene.add(this.character);
     }
 
-
-    // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
+        // Make sure the camera always aims at the sprite
+        this.cameraControls.setTargetPoint(this.character.position);
+
+        // Update the camera orbit controls
+        this.cameraControls.update(deltaTime);
+
+        this.character.update(deltaTime);
+    }
+
+    onKeyDown(event: KeyboardEvent): void 
+    {
+        if(event.key == "d" || event.key == "ArrowRight")
+        {
+            this.character.moveDirection.x = 1;
+        }
+        else if(event.key == "a" || event.key == "ArrowLeft")
+        {
+            this.character.moveDirection.x = -1;
+        }
+
+        if(event.key == "w" || event.key == "ArrowUp")
+        {
+            this.character.moveDirection.y = 1;
+        }
+        else if(event.key == "s" || event.key == "ArrowDown")
+        {
+            this.character.moveDirection.y = -1;
+        }
+    }
+
+    onKeyUp(event: KeyboardEvent): void 
+    {
+        if(event.key == "d" || event.key == "ArrowRight")
+        {
+            if(this.character.moveDirection.x == 1)
+                this.character.moveDirection.x = 0;
+        }
+        else if(event.key == "a" || event.key == "ArrowLeft")
+        {
+            if(this.character.moveDirection.x == -1)
+                this.character.moveDirection.x = 0;
+        }
+
+        if(event.key == "w" || event.key == "ArrowUp")
+        {
+            if(this.character.moveDirection.y == 1)
+                this.character.moveDirection.y = 0;
+        }
+        else if(event.key == "s" || event.key == "ArrowDown")
+        {
+            if(this.character.moveDirection.y == -1)
+                this.character.moveDirection.y = 0;
+        }
     }
 }
---
+++ src/SpriteCharacter.ts
@@ -0,0 +1,158 @@
+/** CSci-4611 Example Code
+ * Copyright 2023+ Regents of the University of Minnesota
+ * Please do not distribute beyond the CSci-4611 course
+ * 
+ * This example created by Prof. Evan Suma Rosenberg.
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */
+
+import * as gfx from 'gophergfx'
+
+export class SpriteCharacter extends gfx.Mesh3
+{
+    public moveDirection: gfx.Vector2;
+    public walkSpeed: number;
+
+    private spriteMaterial: gfx.UnlitMaterial;
+    private fps: number;
+
+    private rightWalkTextures: gfx.Texture[];
+    private frontWalkTextures: gfx.Texture[];
+    private backWalkTextures: gfx.Texture[];
+
+    private currentFrame: number;
+    private lastFrameTime: number;
+
+    constructor(width: number, height: number, bottomPadding: number, fps: number)
+    {
+        super();
+
+        // Hack because we exceeded the number of textures
+        this.fps = fps / 3;
+
+        const vertices: number[] = [];
+        vertices.push(-width/2, -bottomPadding, 0);
+        vertices.push(width/2, -bottomPadding, 0);
+        vertices.push(width/2, height - bottomPadding, 0);
+        vertices.push(-width/2, height - bottomPadding,0);
+
+        const normals: number[] = [];
+        normals.push(0, 0, -1);
+        normals.push(0, 0, -1);
+        normals.push(0, 0, -1);
+        normals.push(0, 0, -1);
+        
+        const indices: number[] = [];
+        indices.push(0, 2, 1);
+        indices.push(2, 0, 3);
+
+        const uvs: number[] = [];
+        uvs.push(1, 1);
+        uvs.push(0, 1);
+        uvs.push(0, 0);
+        uvs.push(1, 0);
+
+        this.setVertices(vertices);
+        this.setNormals(normals);
+        this.setIndices(indices);
+        this.setTextureCoordinates(uvs);
+        this.createDefaultVertexColors();
+
+        this.frontWalkTextures = [];
+        for(let i=0; i < this.fps; i++)
+        {
+            if(i < 4)
+                this.frontWalkTextures[i] = new gfx.Texture('./assets/sprites/HumanoidCharacter-Front-walk_0' + i*3 + '.png');
+            else
+                this.frontWalkTextures[i] = new gfx.Texture('./assets/sprites/HumanoidCharacter-Front-walk_' + i*3 + '.png');
+        }
+
+        this.backWalkTextures = [];
+        for(let i=0; i < this.fps; i++)
+        {
+            if(i < 4)
+                this.backWalkTextures[i] = new gfx.Texture('./assets/sprites/HumanoidCharacter-Back-walk_0' + i*3 + '.png');
+            else
+                this.backWalkTextures[i] = new gfx.Texture('./assets/sprites/HumanoidCharacter-Back-walk_' + i*3 + '.png');
+        }
+
+        this.rightWalkTextures = [];
+        for(let i=0; i < this.fps; i++)
+        {
+            if(i < 4)
+                this.rightWalkTextures[i] = new gfx.Texture('./assets/sprites/HumanoidCharacter-Right-walk_0' + i*3 + '.png');
+            else
+                this.rightWalkTextures[i] = new gfx.Texture('./assets/sprites/HumanoidCharacter-Right-walk_' + i*3 + '.png');
+        }
+
+        this.spriteMaterial = new gfx.UnlitMaterial();
+        this.spriteMaterial.side = gfx.Side.DOUBLE;
+        this.material = this.spriteMaterial;
+        this.spriteMaterial.texture = this.frontWalkTextures[0];
+
+        this.moveDirection = new gfx.Vector2();
+        this.currentFrame = 0;
+        this.lastFrameTime = 0;
+        this.walkSpeed = 1.5;
+    }
+
+    update(deltaTime: number): void
+    {
+        const camera = gfx.GfxApp.getInstance().camera;
+        this.lookAt(camera.position);
+
+        if(this.moveDirection.length() == 0)
+            return;
+
+        const currentTime = Date.now() / 1000;
+        if(currentTime - this.lastFrameTime > 1 / this.fps)
+        {
+            if(this.moveDirection.x > 0)
+            {
+                this.scale.x = Math.abs(this.scale.x);
+                this.advanceFrame(this.rightWalkTextures);
+            }
+            else if(this.moveDirection.x < 0)
+            {
+                this.scale.x = -Math.abs(this.scale.x);
+                this.advanceFrame(this.rightWalkTextures);
+            }
+            else if(this.moveDirection.y > 0)
+            {
+                this.advanceFrame(this.backWalkTextures);
+            }
+            else if(this.moveDirection.y < 0)
+            {
+                this.advanceFrame(this.frontWalkTextures);
+            }
+
+            this.lastFrameTime = currentTime;
+        }
+
+        // Get the character's walking velocity in world space
+        const velocity = new gfx.Vector3(this.moveDirection.x, 0, -this.moveDirection.y);
+
+        // Rotate in the direction of the camera
+        velocity.rotate(camera.rotation);
+
+        // Zero out any change in height
+        velocity.y = 0;
+
+        // Normalize and scale by the walk speed
+        velocity.normalize();
+        velocity.multiplyScalar(this.walkSpeed * deltaTime);
+
+        // Add to the character's root position
+        this.position.add(velocity);
+    }
+
+    private advanceFrame(textures: gfx.Texture[]): void
+    {
+        this.currentFrame++;
+
+        if(this.currentFrame >= textures.length)
+            this.currentFrame = 0;
+        
+        this.spriteMaterial.texture = textures[this.currentFrame];
+    }
+}
